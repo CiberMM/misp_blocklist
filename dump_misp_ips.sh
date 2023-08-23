dc01cca17b1e98b6580467c4d259d97ee6075cfe
CiberMM <ciberseguridad@masmovil.com>
2023-08-23 13:19:17 +0200

    borrados:        README

--- dump_misp_ips.sh
+++ dump_misp_ips.sh
@@ -2,7 +2,7 @@
 
 python3 -W ignore dump_misp_ips.py | sort -nu > misp_blocklist.txt
 git add misp_blocklist.txt
-export DATE=`date +%Y%m%d`
+export DATE=`date +%Y%m%d%HH%MM`
 export GITHUB_TOKEN=`cat github_CiberMM_token.txt`
 git commit -m "last update $DATE"
 git push -u origin main
