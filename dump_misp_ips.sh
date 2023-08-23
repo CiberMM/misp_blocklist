721326a3ad8cf8cc2fcd99f3fe12760b0d339f11
Ciberseguridad <ciberseguridad@KCIBER01.masmovil.com>
2023-08-23 09:58:30 +0200

    modificados:     dump_misp_ips.sh

--- dump_misp_ips.sh
+++ dump_misp_ips.sh
@@ -3,5 +3,6 @@
 python3 -W ignore dump_misp_ips.py | sort -nu > misp_blocklist.txt
 git add misp_blocklist.txt
 export DATE=`date +%Y%m%d`
+export GITHUB_TOKEN=`cat github_CiberMM_token.txt`
 git commit -m "last update $DATE"
 git push -u origin main
