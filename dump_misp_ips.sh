457017fdbb9adff5417d2edd084c0a24ea4db5a2
CiberMM <ciberseguridad@masmovil.com>
2023-08-23 14:51:41 +0200

    modificados:     dump_misp_ips.sh

--- dump_misp_ips.sh
+++ dump_misp_ips.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /var/tmp/workspace/misp_blacklist
+cd /var/tmp/workspace/misp_blocklist
 python3 -W ignore dump_misp_ips.py | sort -nu > misp_blocklist.txt
 git add misp_blocklist.txt
 export DATE=`date +%Y%m%d%H%M`
