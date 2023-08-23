4197d1f75af93f1a789695fa9390662e419a1613
CiberMM <ciberseguridad@masmovil.com>
2023-08-23 13:47:40 +0200

    modificados:     dump_misp_ips_nokey.py

--- dump_misp_ips_nokey.py
+++ dump_misp_ips_nokey.py
@@ -5,7 +5,7 @@ import json
 
 MISP_API_KEY = '<YOUR MISP API KEY>'
 headers = {'Accept': 'application/json', 'Content-type': 'application/json', 'Authorization': MISP_API_KEY}
-r = requests.get('https://172.30.158.48/attributes/restSearch/returnFormat:json/type:ip-src', headers=headers, verify=False)
+r = requests.get('https://<MISP IP>/attributes/restSearch/returnFormat:json/type:ip-src', headers=headers, verify=False)
 response = r.json()
 for item in response["response"]["Attribute"]:
     print(item['value'])
